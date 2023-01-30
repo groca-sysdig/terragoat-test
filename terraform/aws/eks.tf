e6cebe8fae3bacee58253d0eb9c7e74dfde2da60
groca-sysdig <105215194+groca-sysdig@users.noreply.github.com>
2023-01-30 16:34:54 +0100

    Update eks.tf

--- terraform/aws/eks.tf
+++ terraform/aws/eks.tf
@@ -41,7 +41,7 @@ resource aws_iam_role_policy_attachment "policy_attachment-AmazonEKSServicePolic
 }
 
 resource aws_vpc "eks_vpc" {
-  cidr_block           = "10.10.0.0/16"
+  cidr_block           = "10.20.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support   = true
   tags = merge({
